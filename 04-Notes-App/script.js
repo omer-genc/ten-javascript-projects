499214237050a9f62734625d5cffe587236e26a2
omer genc <omergnc1@gmail.com>
2021-08-16 02:37:39 +0300

    edit note not finished

--- 04-Notes-App/script.js
+++ 04-Notes-App/script.js
@@ -1,10 +1,10 @@
 let rowELEMENT = document.querySelector("#row");
 let newNote = `
-<div id = "note-container" class="card border-0 shadow rounded  mt-4 p-0" style="max-width: 18rem;">
+<div id="note-container" class="card border-0 shadow rounded  mt-4 p-0" style="max-width: 18rem;">
 
     <div class="card-header card-header2 d-flex justify-content-between">
-        <i class="fa fa-edit btn" style="color: white;"></i>
-        <i onclick="remove(this)" class="fa fa-trash btn" style="color: white;" ></i>
+        <i onclick="edit(this)" class="fa fa-edit btn" style="color: white;"></i>
+        <i onclick="remove(this)" class="fa fa-trash btn" style="color: white;"></i>
     </div>
 
     <div class="card-body text-success">
@@ -21,8 +21,29 @@ function addNewNote(){
 }
 
 function remove(element){
-    element.parentElement.parentElement.remove()
-    console.log(element)
+    element.parentElement.parentElement.remove();
+    console.log(element);
     // document.getElementById(`${id}`).remove();
     // document.querySelector(id).remove();
 }
+
+function edit(element){
+    let baslik = prompt("Not Başlığı giriniz")
+    let not = prompt("Not Başlığı giriniz")
+    element.parentElement.parentElement.innerHTML = `
+    <div id="note-container" class="card border-0 shadow rounded  mt-4 p-0" style="max-width: 18rem;">
+
+        <div class="card-header card-header2 d-flex justify-content-between">
+          <i onclick="edit(this)" class="fa fa-edit btn" style="color: white;"></i>
+          <i onclick="remove(this)" class="fa fa-trash btn" style="color: white;"></i>
+        </div>
+
+        <div class="card-body text-success">
+        <h5 class="card-title">${baslik}</h5>
+        <p class="card-text">${not}</p>
+        </div>
+
+    </div>
+    `
+}
+
