dd5c08088f6005b7f27bd910ff124222ff03e0a5
omer genc <omergnc1@gmail.com>
2021-08-16 02:52:44 +0300

    p04 finish

--- 04-Notes-App/script.js
+++ 04-Notes-App/script.js
@@ -8,9 +8,8 @@ let newNote = `
     </div>
 
     <div class="card-body text-success">
-        <h5 class="card-title">Success card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-        content.</p>
+        <h5 class="card-title">Not Başlığı</h5>
+        <p class="card-text">Not içeriği</p>
     </div>
 
 </div>
@@ -28,22 +27,25 @@ function remove(element){
 }
 
 function edit(element){
-    let baslik = prompt("Not Başlığı giriniz")
-    let not = prompt("Not Başlığı giriniz")
-    element.parentElement.parentElement.innerHTML = `
-    <div id="note-container" class="card border-0 shadow rounded  mt-4 p-0" style="max-width: 18rem;">
+    let baslik = prompt("Not başlığını giriniz")
+    let not = prompt("Not içeriğini giriniz")
+    if(!(baslik == null || not == null))
+        element.parentElement.parentElement.innerHTML = `
+        
 
-        <div class="card-header card-header2 d-flex justify-content-between">
-          <i onclick="edit(this)" class="fa fa-edit btn" style="color: white;"></i>
-          <i onclick="remove(this)" class="fa fa-trash btn" style="color: white;"></i>
-        </div>
+            <div class="card-header card-header2 d-flex justify-content-between">
+            <i onclick="edit(this)" class="fa fa-edit btn" style="color: white;"></i>
+            <i onclick="remove(this)" class="fa fa-trash btn" style="color: white;"></i>
+            </div>
 
-        <div class="card-body text-success">
-        <h5 class="card-title">${baslik}</h5>
-        <p class="card-text">${not}</p>
-        </div>
+            <div class="card-body text-success">
+            <h5 class="card-title">${baslik}</h5>
+            <p class="card-text">${not}</p>
+            </div>
 
-    </div>
-    `
+        
+        `
+    else
+        alert("Boş veri giremezsiniz")
 }
 
